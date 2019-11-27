85e7bd22d11bd6913697514ca831c429d7e92a24
ThonaLJimenez <ljimenez@thonaseguros.mx>
2019-11-27 16:08:29 -0600

    S19-000-RESERVA
    Objetos nuevos para la carga de información de reserva OPC  (S19-000-RESERVA)

---
+++ Objetos-BD/Scripts/TH_CARGA_OPC_crea_permisos.sql
@@ -0,0 +1,11 @@
+-- CREA SINONIMOS Y GRANST PARA PAQUETES
+-- =============================
+-- Genera los permisos
+-- =============================
+grant EXECUTE on TH_CARGA_OPC to PUBLIC
+;
+-- =============================
+-- Crea el Sinónimo
+-- =============================
+CREATE or replace PUBLIC SYNONYM TH_CARGA_OPC FOR TH_CARGA_OPC
+;
