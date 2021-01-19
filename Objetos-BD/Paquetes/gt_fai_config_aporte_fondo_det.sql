33f5e9a9ff11b87862a4d9199622dee637acf10f
Heber González <hgonzalez@thonaseguros.mx>
2021-01-19 10:42:12 -0600

    Actualiza GT_FAI_CONFIG_APORTE_FONDO_DET
    Se actualiza procedimiento de activacion para que considere estatus correcto en detalle de aportes

--- Objetos-BD/Paquetes/gt_fai_config_aporte_fondo_det.sql
+++ Objetos-BD/Paquetes/gt_fai_config_aporte_fondo_det.sql
@@ -149,7 +149,7 @@ CURSOR APORTES_Q IS
       AND IDetPol      = nIDetPol
       AND CodAsegurado = nCodAsegurado
       AND IdFondo      = nIdFondo
-      AND StsAporte    = 'SOL';
+      AND StsAporte    = 'SOLICI';
 BEGIN
    FOR X IN APORTES_Q LOOP
       UPDATE FAI_CONFIG_APORTE_FONDO_DET
