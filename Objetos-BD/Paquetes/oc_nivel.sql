b1bc1bd5549486454f03d652c36492ffbd544c0d
Valle2020 <alopez@thonaseguros.mx>
2021-06-25 14:10:38 -0500

    Modificacion a Pkg OC_NIVEL
    Se agrega funcionalidad que valida el Prefijo del Usuario Web.

--- Objetos-BD/Paquetes/oc_nivel.sql
+++ Objetos-BD/Paquetes/oc_nivel.sql
@@ -12,6 +12,7 @@ CREATE OR REPLACE PACKAGE SICAS_OC.oc_nivel  IS
    PROCEDURE ACTIVAR(nCodCia NUMBER, nCodNivel NUMBER);
    PROCEDURE ACTIVAR_MOD(nCodCia NUMBER, nCodNivel NUMBER, nNUMCAMBIO NUMBER);
    PROCEDURE SUSPENDER(nCodCia NUMBER, nCodNivel NUMBER);
+   FUNCTION  PREFIJO_USUARIO_WEB(nCodCia NUMBER, nCodNivel NUMBER) RETURN VARCHAR2;
 END OC_NIVEL;
 /
 
@@ -56,10 +57,6 @@ BEGIN
      AND NUMCAMBIO = nNumCambio;
 END ACTIVAR_MOD;
 
-
-
-
-
 PROCEDURE SUSPENDER(nCodCia NUMBER, nCodNivel NUMBER) IS
 BEGIN
    UPDATE NIVEL
@@ -67,5 +64,34 @@ BEGIN
    WHERE CodCia    = nCodCia
      AND CodNivel = nCodNivel;
 END SUSPENDER;
+
+FUNCTION  PREFIJO_USUARIO_WEB(nCodCia NUMBER, nCodNivel NUMBER) RETURN VARCHAR2 IS
+cPrefijoUsuarioWeb NIVEL.PrefijoUsuarioWeb%TYPE;
+BEGIN
+   BEGIN
+      SELECT NVL(PrefijoUsuarioWeb,'SINPREFIJO')
+        INTO cPrefijoUsuarioWeb
+        FROM NIVEL
+       WHERE CodCia     = nCodCia
+         AND CodNivel   = nCodNivel;
+   EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+         cPrefijoUsuarioWeb := 'NO EXISTE';
+   END;
+   RETURN cPrefijoUsuarioWeb;
+END PREFIJO_USUARIO_WEB;
 END OC_NIVEL;
+/
+
+--
+-- OC_NIVEL  (Synonym) 
+--
+--  Dependencies: 
+--   OC_NIVEL (Package)
+--
+CREATE OR REPLACE PUBLIC SYNONYM OC_NIVEL FOR SICAS_OC.OC_NIVEL
+/
+
+
+GRANT EXECUTE ON SICAS_OC.OC_NIVEL TO PUBLIC
 /
