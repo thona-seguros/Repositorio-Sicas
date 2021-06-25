28cc281226e64d80de9a6154470674239ced371e
Valle2020 <alopez@thonaseguros.mx>
2021-06-25 13:49:00 -0500

    INSERT PARAMETROS GLOBALES
    Insercion de URL de WS para alta y Actualizacion de informacion del Agente en plataforma digital.

---
+++ Objetos-BD/Scripts/INSERT_PARAMETROS_GLOBALES.sql
@@ -0,0 +1,5 @@
+INSERT INTO PARAMETROS_GLOBALES
+		VALUES (1, 'A01', 'https://thona-api.azurewebsites.net/WebServiceThonApi.asmx?WSDL', 'WS para Agregar o aAtualiza Usuario de Agente en Plataforma Digital');
+/
+COMMIT;
+/
