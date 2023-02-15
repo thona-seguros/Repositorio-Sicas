d48deca05034cb715f30f8aede2225397df1ea8b
cgarcia-thonaseguros <111448008+cgarcia-thonaseguros@users.noreply.github.com>
2023-02-15 12:21:08 -0600

    Add files via upload

---
+++ Objetos-BD/Disparadores/TRG_ACTUALIZA_CLAUSULAS.sql
@@ -0,0 +1,40 @@
+
+  CREATE OR REPLACE TRIGGER "SICAS_OC"."TRG_ACTUALIZA_CLAUSULAS" 
+ BEFORE
+  UPDATE
+ ON CLAUSULAS_DETALLE
+REFERENCING NEW AS NEW OLD AS OLD
+ FOR EACH ROW
+DECLARE
+  nMaxTransaccion   NUMBER(18,0);
+  cCodcia           NUMBER(14,0);
+  cIdePoliza        NUMBER(14,0);
+  cIP               VARCHAR2(20);
+  cUsuario          VARCHAR2(30);
+  cMoneda           VARCHAR2(25);
+BEGIN
+   --  
+   SELECT SQ_LOGTRANSACC.NEXTVAL
+     INTO nMaxTransaccion
+     FROM DUAL;
+   --           
+    SELECT USER
+    INTO   cUsuario
+    FROM   DUAL;
+
+    SELECT  CODCIA
+    INTO    cCodCia
+    FROM    POLIZAS
+    WHERE   IDPOLIZA = :NEW.IDPOLIZA;
+
+    SELECT SYS_CONTEXT ('USERENV', 'IP_ADDRESS')
+    INTO cIP
+    FROM DUAL;
+            INSERT INTO LOG_TRANSACCION
+              VALUES(nMaxtransaccion,cCodCia,'CLAUSULAS'||' '||TO_CHAR(:NEW.IDPOLIZA),SYSDATE,
+              cIP, 'Codigo de Clausula'||:NEW.COD_CLAUSULA, 'ACTUALIZO CLAUSULAS',
+              'DATOS INGRESADOS: '||' '||'CLAUSULAS: ',cUsuario);
+END;
+
+
+ALTER TRIGGER "SICAS_OC"."TRG_ACTUALIZA_CLAUSULAS" ENABLE
