69d5bf3c0145792b94af763ce6f232ee040387fd
Heber Gonzalez <hgonzalez@thonaseguros.mx>
2023-04-10 11:27:30 -0600

    actualiza GET_POLIZAS_SI
    Se agrega sub grupo

--- Objetos-BD/Vistas/GET_POLIZAS_SI.sql
+++ Objetos-BD/Vistas/GET_POLIZAS_SI.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW GET_POLIZAS_SI (CODCIA,	IDPOLIZA,	IDSINIESTRO,	NOMBRE_BENEFICIARIO,	NOMBRE_CONTRATANTE,	AseguradoSiniestradoSiniestro,
-                            FolioSiniestroPortal, NumPoliza, Fec_Ocurrencia, EstadoOcurrencia, CausaSiniestro, CobertPolizaDesc, Fec_Notificacion
+                            FolioSiniestroPortal, NumPoliza, Fec_Ocurrencia, EstadoOcurrencia, CausaSiniestro, CobertPolizaDesc, Fec_Notificacion,
+                            IDetPol
                             ) AS 
    SELECT SI.CODCIA
          ,SI.IDPOLIZA
@@ -14,12 +15,17 @@ CREATE OR REPLACE VIEW GET_POLIZAS_SI (CODCIA,	IDPOLIZA,	IDSINIESTRO,	NOMBRE_BEN
          ,OC_VALORES_DE_LISTAS.BUSCA_LVALOR('CAUSIN', SI.Motivo_De_Siniestro) CausaSiniestro
          ,PP.DescPoliza CobertPolizaDesc
          ,SI.Fec_Notificacion
+         ,DP.IDetPol
       FROM SINIESTRO SI
          ,POLIZAS PP
+         ,DETALLE_POLIZA DP
       WHERE 1 = 1
            --**AND SI.IDSINIESTRO      = 56521--371235
            --**AND SI.IDPOLIZA = 9746--48946
-      AND PP.IDPOLIZA		    = SI.idpoliza
-      AND PP.CODCIA  			= SI.CODCIA
-      AND PP.CODEMPRESA       = SI.CODEMPRESA;
+      AND DP.IDPOLIZA		    = SI.idpoliza
+      AND DP.CODCIA  			= SI.CODCIA
+      AND DP.CODEMPRESA       = SI.CODEMPRESA
+      AND DP.IDetPol          = SI.IDetPol
+      AND PP.CodCia           = DP.CodCia
+      AND PP.IdPoliza         = DP.IdPoliza;
          
