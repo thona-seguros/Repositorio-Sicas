4627f34f56bb2d71a3fff4ca23b0ffd7c4e43869
Heber Gonzalez <hgonzalez@thonaseguros.mx>
2023-04-10 18:11:07 -0600

    Actualiza vistas de reservas y beneficiario
    Actualiza vistas para obtener reservas y beneficiario

--- Objetos-BD/Vistas/GET_MONTO_RESERVA.sql
+++ Objetos-BD/Vistas/GET_MONTO_RESERVA.sql
@@ -1,25 +1,32 @@
- CREATE VIEW GET_BENEF_SIN (CODCIA,	IDPOLIZA,	IDSINIESTRO,	IDDETSIN,	CODEMPRESA,	CODCOBERT,	MONTO_RESERVA
-
+ CREATE VIEW GET_MONTO_RESERVA (CODCIA,	IDPOLIZA,	IDSINIESTRO,	IDDETSIN,	CODEMPRESA,	CODCOBERT,	
+                                    CodCptoTransac, DescripConcepto, MONTO_RESERVA
 ) AS
-
-
-
- 
- 
- GET_MONTO_RESERVA AS (SELECT CSA.CODCIA
-                                ,CSA.IDPOLIZA
-                                ,CSA.IDSINIESTRO
-                                ,CSA.IDDETSIN
-                                ,CSA.CODEMPRESA
-                                ,CSA.CODCOBERT
-                                ,SUM(CSA.MONTO_RESERVADO_MONEDA) MONTO_RESERVA
-                            FROM COBERTURA_SINIESTRO CSA
-                            WHERE 1 = 1
-                            --**AND CSA.IDSINIESTRO      = 57252
-                            GROUP BY CSA.CODCIA
-                                    ,CSA.IDPOLIZA
-                                    ,CSA.IDSINIESTRO
-                                    ,CSA.IDDETSIN
-                                    ,CSA.CODEMPRESA
-                                    ,CSA.CODCOBERT
-                    )
+SELECT CSA.CODCIA
+      ,CSA.IDPOLIZA
+      ,CSA.IDSINIESTRO
+      ,CSA.IDDETSIN
+      ,CSA.CODEMPRESA
+      ,CSA.CODCOBERT
+      ,CSA.CodCptoTransac
+      ,CC.DescripConcepto
+      ,CASE 
+         WHEN CC.Signo_Concepto = '+' THEN 
+            SUM(CSA.MONTO_RESERVADO_MONEDA)
+         WHEN CC.Signo_Concepto = '-' THEN 
+            SUM(CSA.MONTO_RESERVADO_MONEDA) * -1 
+         ELSE 
+            SUM(CSA.MONTO_RESERVADO_MONEDA) 
+      END MONTO_RESERVA
+  FROM COBERTURA_SINIESTRO CSA, CATALOGO_DE_CONCEPTOS CC
+  WHERE CSA.CodCptoTransac =  CC.CodConcepto
+    --and IDSINIESTRO      = 365085
+  GROUP BY CSA.CODCIA
+      ,CSA.IDPOLIZA
+      ,CSA.IDSINIESTRO
+      ,CSA.IDDETSIN
+      ,CSA.CODEMPRESA
+      ,CSA.CODCOBERT   
+      ,CSA.CodCptoTransac
+      ,CC.Signo_Concepto
+      ,CC.DescripConcepto
+                    
--- Objetos-BD/Vistas/GET_MONTO_RESERVA_ASEG.sql
+++ Objetos-BD/Vistas/GET_MONTO_RESERVA_ASEG.sql
@@ -1,20 +1,32 @@
-CREATE VIEW GET_MONTO_RESERVA_ASEG (CODCIA,	IDPOLIZA,	IDSINIESTRO,	IDDETSIN,	CODEMPRESA,	CODCOBERT,	MONTO_RESERVA
-
+CREATE OR REPLACE VIEW GET_MONTO_RESERVA_ASEG (CODCIA,	IDPOLIZA,	IDSINIESTRO,	IDDETSIN,	CODEMPRESA,	CODCOBERT,	
+                                    CodCptoTransac, DescripConcepto, MONTO_RESERVA
 ) AS
-
- GET_MONTO_RESERVA_ASEG AS (SELECT CSA.CODCIA
-                                    ,CSA.IDPOLIZA
-                                    ,CSA.IDSINIESTRO
-                                    ,CSA.IDDETSIN
-                                    ,CSA.CODEMPRESA
-                                    ,CSA.CODCOBERT
-                                    ,SUM(CSA.MONTO_RESERVADO_MONEDA) MONTO_RESERVA
-                                FROM COBERTURA_SINIESTRO_ASEG CSA
-                                WHERE 1 = 1
-                                GROUP BY CSA.CODCIA
-                                    ,CSA.IDPOLIZA
-                                    ,CSA.IDSINIESTRO
-                                    ,CSA.IDDETSIN
-                                    ,CSA.CODEMPRESA
-                                    ,CSA.CODCOBERT
-                    )
+SELECT CSA.CODCIA
+      ,CSA.IDPOLIZA
+      ,CSA.IDSINIESTRO
+      ,CSA.IDDETSIN
+      ,CSA.CODEMPRESA
+      ,CSA.CODCOBERT
+      ,CSA.CodCptoTransac
+      ,CC.DescripConcepto
+      ,CASE 
+         WHEN CC.Signo_Concepto = '+' THEN 
+            SUM(CSA.MONTO_RESERVADO_MONEDA)
+         WHEN CC.Signo_Concepto = '-' THEN 
+            SUM(CSA.MONTO_RESERVADO_MONEDA) * -1 
+         ELSE 
+            SUM(CSA.MONTO_RESERVADO_MONEDA) 
+      END MONTO_RESERVA
+  FROM COBERTURA_SINIESTRO_ASEG CSA, CATALOGO_DE_CONCEPTOS CC
+  WHERE CSA.CodCptoTransac =  CC.CodConcepto
+    --and IDSINIESTRO      = 365085
+  GROUP BY CSA.CODCIA
+      ,CSA.IDPOLIZA
+      ,CSA.IDSINIESTRO
+      ,CSA.IDDETSIN
+      ,CSA.CODEMPRESA
+      ,CSA.CODCOBERT   
+      ,CSA.CodCptoTransac
+      ,CC.Signo_Concepto
+      ,CC.DescripConcepto
+                    
