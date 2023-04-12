3077041acefdc25fbbcadc5e3f881031129e60ae
Heber Gonzalez <hgonzalez@thonaseguros.mx>
2023-04-12 16:00:40 -0600

    actualización vistas para extraccion pago siniestros
    Optimización de vistas

--- Objetos-BD/Vistas/GET_MONTO_PAGO_ASEG.sql
+++ Objetos-BD/Vistas/GET_MONTO_PAGO_ASEG.sql
@@ -1,51 +1,58 @@
- 
-CREATE VIEW GET_MONTO_PAGO_ASEG (CODCIA,	CODEMPRESA,	IDPOLIZA,	IDSINIESTRO,	IDTRANSACCION,	NUM_APROBACION,	BENEF,	IDDETSIN,	STSAPROBACION,	FECHA_TRANSACCION,	USUARIO_GENERO,	MONTO_PAGO
+CREATE OR REPLACE VIEW GET_MONTO_PAGO_ASEG (CODCIA,	CODEMPRESA,	IDPOLIZA,	IDSINIESTRO,	IDTRANSACCION,	NUM_APROBACION,	BENEF,	IDDETSIN,	
+                                 STSAPROBACION,	FECHA_TRANSACCION,	USUARIO_GENERO,	MONTO_PAGO, Cod_Pago, CodCptoTransac
 ) AS
 
- GET_MONTO_PAGO_ASEG AS (SELECT A.CODCIA
-                                ,A.CODEMPRESA
-                                ,A.IDPOLIZA
-                                ,A.IDSINIESTRO
-                                ,A.IDTRANSACCION
-                                ,A.NUM_APROBACION
-                                ,A.BENEF
-                                ,A.IDDETSIN
-                                ,A.STSAPROBACION
-                                ,TO_CHAR(T.FECHATRANSACCION,'DD/MM/YYYY') FECHA_TRANSACCION
-                                ,T.USUARIOGENERO USUARIO_GENERO
-                                ,SUM(A.MONTO_MONEDA) MONTO_PAGO
-                            FROM TRANSACCION T
-                                ,(SELECT PAU.CODCIA, PAU.CODUSUARIO
-                                FROM PROCESO_AUTORIZA_USUARIO PAU
-                                WHERE 1 = 1
-                                AND PAU.CODUSUARIO          IS NOT NULL
-                                AND PAU.CODCIA      		= 1--T.CODCIA
-                                AND PAU.CODPROCESO  		= 9040
-                                AND PAU.idtiposeg   		= 'NOAPLI') GU
-                                ,APROBACION_ASEG A
-                                --??,DETALLE_APROBACION DA
-                            WHERE 1 = 1
-                            AND T.USUARIOGENERO		= GU.CODUSUARIO
-                            AND T.CODCIA            = GU.CODCIA
-                            AND T.IDPROCESO			= 6
-                            AND A.CODCIA            = T.CODCIA
-                            AND A.CODEMPRESA        = T.CODEMPRESA
-                            AND A.IDTRANSACCION     = T.IDTRANSACCION
-                            AND A.STSAPROBACION     = 'PAG'
-                            --??AND A.CODCIA   			= DA.CODCIA
-                            --??AND A.CODEMPRESA        = DA.CODEMPRESA
-                            --??AND A.IDSINIESTRO       = DA.IDSINIESTRO
-                            --??AND A.NUM_APROBACION    = DA.NUM_APROBACION
-                            GROUP BY A.CODCIA
-                                ,A.CODEMPRESA
-                                ,A.IDTRANSACCION
-                                ,A.IDPOLIZA
-                                ,A.IDSINIESTRO
-                                ,A.BENEF
-                                ,A.IDDETSIN
-                                ,A.NUM_APROBACION
-                                ,A.STSAPROBACION
-                                ,TO_CHAR(T.FECHATRANSACCION,'DD/MM/YYYY')
-                                ,T.USUARIOGENERO
-                            ORDER BY 3,4,8
-						)
+SELECT /*+ PARALLEL */
+       A.CODCIA
+       ,A.CODEMPRESA
+       ,A.IDPOLIZA
+       ,A.IDSINIESTRO
+       ,A.IDTRANSACCION
+       ,A.NUM_APROBACION
+       ,A.BENEF
+       ,A.IDDETSIN
+       ,A.STSAPROBACION
+       ,TO_CHAR(T.FECHATRANSACCION,'DD/MM/YYYY') FECHA_TRANSACCION
+       ,T.USUARIOGENERO USUARIO_GENERO
+       --,SUM(A.MONTO_MONEDA) MONTO_PAGO
+       ,SUM(DA.MONTO_LOCAL) MONTO_PAGO
+       ,DA.Cod_Pago
+       ,DA.CodCptoTransac
+  FROM TRANSACCION T
+       ,APROBACION_ASEG A
+       ,DETALLE_APROBACION_ASEG DA
+       ,Config_Transac_Siniestros TS
+       ,Cptos_Transac_Siniestros CTS
+ WHERE T.IDPROCESO			= 6
+   AND A.CODCIA            = T.CODCIA
+   AND A.CODEMPRESA        = T.CODEMPRESA
+   AND A.IDTRANSACCION     = T.IDTRANSACCION
+   AND A.STSAPROBACION     = 'PAG'
+   AND A.CODCIA   			= DA.CODCIA
+   AND A.CODEMPRESA        = DA.CODEMPRESA
+   AND A.IDSINIESTRO       = DA.IDSINIESTRO
+   AND A.NUM_APROBACION    = DA.NUM_APROBACION
+   AND DA.CodCia           = TS.CodCia
+   AND DA.CodTransac       = TS.CodTransac
+   AND DA.CodCia           = CTS.CodCia
+   AND DA.CodTransac       = CTS.CodTransac
+   AND DA.CodCptoTransac   = CTS.CodCptoTransac
+   --AND CTS. IndDisminRva   = 'S'
+   AND TS.StsTransac       = 'ACTIVA'
+   AND TS.CodCia           = CTS.CodCia
+   AND TS.CodTransac       = CTS.CodTransac
+   --AND A.IDSINIESTRO       = 365085
+ GROUP BY A.CODCIA
+       ,A.CODEMPRESA
+       ,A.IDTRANSACCION
+       ,A.IDPOLIZA
+       ,A.IDSINIESTRO
+       ,A.BENEF
+       ,A.IDDETSIN
+       ,A.NUM_APROBACION
+       ,A.STSAPROBACION
+       ,TO_CHAR(T.FECHATRANSACCION,'DD/MM/YYYY')
+       ,T.USUARIOGENERO
+       ,DA.Cod_Pago
+       ,DA.CodCptoTransac
+ ORDER BY 3,4,8;
--- Objetos-BD/Vistas/GET_POLIZAS_SI.sql
+++ Objetos-BD/Vistas/GET_POLIZAS_SI.sql
@@ -1,32 +1,52 @@
 CREATE OR REPLACE VIEW GET_POLIZAS_SI (CODCIA,	IDPOLIZA,	IDSINIESTRO,	NOMBRE_BENEFICIARIO,	NOMBRE_CONTRATANTE,	AseguradoSiniestradoSiniestro,
-                            FolioSiniestroPortal, NumPoliza, Fec_Ocurrencia, EstadoOcurrencia, CausaSiniestro, CobertPolizaDesc, Fec_Notificacion,
-                            IDetPol
+                           CURPAsegurado, FolioSiniestroPortal, NumPoliza, Fec_Ocurrencia, EstadoOcurrencia, CausaSiniestro, SubCausaSiniestro, CobertPolizaDesc, 
+                            Fec_Notificacion, IDetPol, FecEmision, FecIniVig, FecFinVig, Desc_Siniestro, Agente, EjecutivoComercial, Nom_Medico_Certifica,
+                            Hospital
                             ) AS 
-   SELECT SI.CODCIA
+   SELECT /*+ PARALLEL */
+         SI.CODCIA
          ,SI.IDPOLIZA
          ,SI.IDSINIESTRO
          ,OC_CLIENTES.NOMBRE_CLIENTE(PP.CODCLIENTE)           		NOMBRE_BENEFICIARIO
          ,OC_CLIENTES.NOMBRE_CLIENTE(PP.CODCLIENTE)           		NOMBRE_CONTRATANTE
          ,OC_ASEGURADO.NOMBRE_ASEGURADO(PP.CODCIA, PP.CODEMPRESA, SI.COD_ASEGURADO) AseguradoSiniestradoSiniestro
+         ,OC_PERSONA_NATURAL_JURIDICA.CLAVE_CURP(A.Tipo_Doc_Identificacion, A.Num_Doc_Identificacion) CURPAsegurado
          ,SI.NumSiniRef FolioSiniestroPortal
          ,PP.NumPolUnico NumPoliza
          ,SI.Fec_Ocurrencia
          ,OC_PROVINCIA.NOMBRE_PROVINCIA(SI.CodPaisOcurr, SI.CodProvOcurr) EstadoOcurrencia
          ,OC_VALORES_DE_LISTAS.BUSCA_LVALOR('CAUSIN', SI.Motivo_De_Siniestro) CausaSiniestro
+         ,OC_SAI_CAT_GENERAL.DESCRIPCION (9, SI.Motivo_De_Siniestro, si.SubMotivo_Siniestro) SubCausaSiniestro
          ,PP.DescPoliza CobertPolizaDesc
          ,SI.Fec_Notificacion
          ,DP.IDetPol
+         ,PP.FecEmision
+         ,PP.FecIniVig
+         ,PP.FecFinVig
+         ,SI.Desc_Siniestro
+         ,OC_AGENTES.NOMBRE_AGENTE(PP.CodCia, AP.Cod_Agente) Agente
+         ,OC_EJECUTIVO_COMERCIAL.NOMBRE_EJECUTIVO(PP.CodCia, OC_AGENTES.EJECUTIVO_COMERCIAL(PP.CodCia, AP.Cod_Agente)) EjecutivoComercial
+         ,SI.Nom_Medico_Certifica   
+         ,OC_PROVEEDORES.NOMBRE_PROVEEDOR(SI.CodCia, SI.CodProveedor) Hospital
       FROM SINIESTRO SI
          ,POLIZAS PP
          ,DETALLE_POLIZA DP
+         ,AGENTE_POLIZA AP
+         ,ASEGURADO A
       WHERE 1 = 1
            --**AND SI.IDSINIESTRO      = 56521--371235
-           --**AND SI.IDPOLIZA = 9746--48946
+           --AND PP.Idpoliza = 35425
       AND SI.Sts_Siniestro   != 'SOL'
+      AND AP.Ind_Principal    = 'S'
       AND DP.IDPOLIZA		   = SI.idpoliza
       AND DP.CODCIA  			= SI.CODCIA
       AND DP.CODEMPRESA       = SI.CODEMPRESA
       AND DP.IDetPol          = SI.IDetPol
       AND PP.CodCia           = DP.CodCia
-      AND PP.IdPoliza         = DP.IdPoliza;
+      AND PP.IdPoliza         = DP.IdPoliza
+      AND PP.CodCia           = AP.CodCia
+      AND PP.IdPoliza         = AP.IdPoliza
+      AND SI.CodCia           = A.CodCia
+      AND SI.CodEmpresa       = A.CodEmpresa
+      AND SI.Cod_Asegurado    = A.Cod_Asegurado
          
