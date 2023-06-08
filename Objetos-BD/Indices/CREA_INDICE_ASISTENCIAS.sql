08971a8e0c44d92fa14106403300864bef9d0aa4
ignacio castillo <icastillo@thonaseguros.com>
2023-06-08 15:51:18 -0600

    Ajustes a forma REPOPOLI
    Programacion de asistencias por paquete

---
+++ Objetos-BD/Indices/CREA_INDICE_ASISTENCIAS.sql
@@ -0,0 +1,7 @@
+-- =============================
+-- Genera Indice
+-- =============================
+create  index ASISTENCIAS_IDX_2 on ASISTENCIAS(ID_PAQUETE,STSASISTENCIA)
+  tablespace TS_SICASOC
+;
+
