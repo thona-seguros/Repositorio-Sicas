8bc5e0e0c963408776e4ff3eba0ec89b7626b533
Maria de la Luz Jimenez Silva <mluz@thona.seguros>
2024-02-13 19:25:54 -0600

    Se corrigen los objetos OC_REPOFACT y PROC_REPR20
    Se corrigen los objetos OC_REPOFACT y PROC_REPR20 se parametrizaron los correos de envío y se agregaron a los reportes de emisión, cancelación y pagos el IVA de compensaciones que no estaba considerando

--- Objetos-BD/Procedimientos/PROC_REPR20.sql
+++ Objetos-BD/Procedimientos/PROC_REPR20.sql
@@ -1,14 +1,14 @@
-PROCEDURE PROC_REPR20  IS
-    codcia          NUMBER        := 1; 
-    codempresa      NUMBER        := 1; 
-    PcIdTipoSeg     VARCHAR2(100) := NULL; 
-    PcPlanCob       VARCHAR2(100) := NULL; 
-    PcCodMoneda     VARCHAR2(100) := NULL; 
-    PcCodAgente     VARCHAR2(100) := NULL; 
-    dFecDesde       DATE :=  SYSDATE-1; 
-    dFecHasta       DATE :=  SYSDATE-1; 
+CREATE OR REPLACE PROCEDURE PROC_REPR20  IS
+    codcia          NUMBER        := 1;
+    codempresa      NUMBER        := 1;
+    PcIdTipoSeg     VARCHAR2(100) := NULL;
+    PcPlanCob       VARCHAR2(100) := NULL;
+    PcCodMoneda     VARCHAR2(100) := NULL;
+    PcCodAgente     VARCHAR2(100) := NULL;
+    dFecDesde       DATE :=  SYSDATE-1;
+    dFecHasta       DATE :=  SYSDATE-1;
     Formato         VARCHAR2(100) := 'REGISTRO';
-    CORREOS         VARCHAR2(100) := 'i.gomez@darbrain.com;rmartinez@thonaseguros.mx;jcastillo@thonaseguros.mx';
+    CORREOS         VARCHAR2(100) ; --:= 'i.gomez@darbrain.com;rmartinez@thonaseguros.mx;jcastillo@thonaseguros.mx';
     w_ID_ENVIO      NUMBER;
 BEGIN
     OC_REPOFACT.PRIMA_NETA_COB      (codcia, codempresa, PcIdTipoSeg, PcPlanCob, PcCodMoneda, PcCodAgente, dFecDesde, dFecHasta, Formato, CORREOS, w_ID_ENVIO);
@@ -19,3 +19,4 @@ BEGIN
 EXCEPTION WHEN OTHERS THEN
    RAISE_APPLICATION_ERROR(-20000, 'Error en el Job de PROC_REPR20 ' || SQLERRM);
 END PROC_REPR20;
+/
